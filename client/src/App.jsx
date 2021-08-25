daee69fd97826e6b2ea6792eeab75efff9536f89
Ognjen Gojkovic <ognjen1804@gmail.com>
2021-08-25 11:35:11 +0200

    products component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Products from "./components/products/Products";
+
+import data from "./data.json";
 
 const App = () => {
+  const [state, setState] = React.useState({
+    products: data.products,
+  });
   return (
     <div className="grid-container">
       <header>
         <Link to="/">React Shopping Cart</Link>
       </header>
-      <main>Product List</main>
+      <main>
+        <div className="content">
+          <div className="main">
+            <Products products={state.products} />
+          </div>
+          <div className="sidebar">Cart Items</div>
+        </div>
+      </main>
       <footer>All Rights Reserved.</footer>
     </div>
   );
