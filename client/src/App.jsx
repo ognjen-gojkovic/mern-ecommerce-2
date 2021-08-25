0dcba26d56d3cf062526eba8f0fe769c8b4b21cd
Ognjen Gojkovic <ognjen1804@gmail.com>
2021-08-25 12:33:25 +0200

    filter component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+// components
+import Filter from "./components/filter/Filter";
 import Products from "./components/products/Products";
 
 import data from "./data.json";
@@ -7,7 +10,48 @@ import data from "./data.json";
 const App = () => {
   const [state, setState] = React.useState({
     products: data.products,
+    size: "",
+    sort: "",
   });
+
+  const sortProductsHandler = (e) => {
+    const sort = e.target.value;
+    setState({
+      sort,
+      products: state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        ),
+    });
+  };
+
+  const filterProductsHandler = (e) => {
+    if (e.target.value === "") {
+      setState({
+        size: e.target.value,
+        products: data.products,
+      });
+    } else {
+      setState({
+        size: e.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(e.target.value) >= 0
+        ),
+      });
+    }
+  };
+
   return (
     <div className="grid-container">
       <header>
@@ -16,6 +60,13 @@ const App = () => {
       <main>
         <div className="content">
           <div className="main">
+            <Filter
+              count={state.products.length}
+              size={state.size}
+              sort={state.sort}
+              filterProductsHandler={filterProductsHandler}
+              sortProductsHandler={sortProductsHandler}
+            />
             <Products products={state.products} />
           </div>
           <div className="sidebar">Cart Items</div>
---
+++ client/src/components/filter/Filter.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Filter.css";
+
+const Filter = ({
+  count,
+  sort,
+  size,
+  filterProductsHandler,
+  sortProductsHandler,
+}) => {
+  return (
+    <div className="filter">
+      <div className="filter-result">{count} Products</div>
+      <div className="filter-sort">
+        Order:
+        <select value={sort} onChange={sortProductsHandler}>
+          <option>Latest</option>
+          <option value="lowest">Lowest</option>
+          <option value="highest">Highest</option>
+        </select>
+      </div>
+      <div className="filter-size">
+        Filter:
+        <select value={size} onChange={filterProductsHandler}>
+          <option value="">All</option>
+          <option value="XS">XS</option>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
+          <option value="XXL">XXL</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
